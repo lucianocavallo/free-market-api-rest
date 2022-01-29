62392a532916bacffc62f26e2de71349629c7e56
luchx88 <lucianocavallo@gmail.com>
2022-01-29 01:17:03 -0300

    [fix] trying with another uri

--- src/utils/cors/index.ts
+++ src/utils/cors/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 const whitelist = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
+  "http://localhost:8080",
   "https://free-market-restful-api.herokuapp.com",
 ];
 
